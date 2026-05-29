941815188261b7020ef3a4a88fd656685de14f4f
solivalle <127651078+solivalle@users.noreply.github.com>
2026-05-28 19:34:00 -0600

    Task 2 - VPC and Subnets

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.environment}-public-${count.index + 1}"
+    Environment = var.environment
+    Tier        = "public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = {
+    Name        = "${var.environment}-private-${count.index + 1}"
+    Environment = var.environment
+    Tier        = "private"
+  }
+}
