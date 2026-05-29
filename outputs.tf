cb3ef0cddb4fcb5fddd5096116e0a2ce8ca07117
solivalle <127651078+solivalle@users.noreply.github.com>
2026-05-28 19:41:18 -0600

    Task 5 - Outputs

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  value       = aws_vpc.this.id
+  description = "ID of the VPC"
+}
+
+output "public_subnet_ids" {
+  value       = aws_subnet.public[*].id
+  description = "IDs of the public subnets"
+}
+
+output "private_subnet_ids" {
+  value       = aws_subnet.private[*].id
+  description = "IDs of the private (app) subnets"
+}
+
+output "web_sg_id" {
+  value       = aws_security_group.web.id
+  description = "ID of the web-tier security group"
+}
+
+output "app_sg_id" {
+  value       = aws_security_group.app.id
+  description = "ID of the app-tier security group"
+}
