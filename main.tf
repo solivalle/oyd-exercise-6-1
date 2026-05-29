11a41f915f283f4a4f2ac540011dff9d83f8226a
solivalle <127651078+solivalle@users.noreply.github.com>
2026-05-28 19:37:38 -0600

    Task 3 - Internet Gateway and Public Route Table

--- main.tf
+++ main.tf
@@ -37,3 +37,33 @@ resource "aws_subnet" "private" {
     Tier        = "private"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name        = "${var.environment}-igw"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name        = "${var.environment}-public-rt"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
