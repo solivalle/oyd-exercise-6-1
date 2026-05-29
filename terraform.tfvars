906c234ac268a58dbe303a7918fc05ea4e3ff4c0
solivalle <127651078+solivalle@users.noreply.github.com>
2026-05-28 19:28:07 -0600

    Task 1 - Variables

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+vpc_name = "dev"
+environment = "dev"
+region = "us-west-2"
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "vpc_name" {
+  type        = string
+  description = "Name of the VPC to create"
+}
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+}
+variable "environment" {
+  type        = string
+  description = "The environment for which to create the RDS instance (e.g., dev, staging, prod)"
+}
+variable "region" {
+  type        = string
+  default     = "us-west-2"
+  description = "AWS region"
+}
+variable "azs" {
+  type        = list(string)
+  default = [ "us-east-1a","us-east-1b" ]
+  description = "List of availability zones to deploy subnets into"
+}
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks for public subnets (one per availability zone)"
+}
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks for private (app) subnets (one per availability zone)"
+}
